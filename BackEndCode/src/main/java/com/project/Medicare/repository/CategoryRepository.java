8d62d3e4f62e7944ecdf9f5b1ba74f13e4789642
imrohitroy23 <imrohitroy23@gmail.com>
2023-10-04 21:45:05 +0530

    commit

---
+++ BackEndCode/src/main/java/com/project/Medicare/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.project.Medicare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.Medicare.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+		
+	
+	
+}
+
